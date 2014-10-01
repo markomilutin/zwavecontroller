d8e330c0036bbe01034fa8ead6554f142b29aeac
Marko Milutinovic <markomilutin@gmail.com>
2014-09-30 22:02:49 -0600

    1. Added base message class implementation and tests

--- test/test.basemessage.js
+++ test/test.basemessage.js
@@ -278,42 +278,81 @@ suite('BaseMessage', function() {
 
       done();
     });
-  test('generateMessageBuffer invalid message' +
-       '\n\tPurpose: Attempt to generate the message buffer when data is not valid' +
-       '\n\tExpectation: An Error should be returned',
+  test('generateMessageBuffer valid message' +
+       '\n\tPurpose: Attempt to generate the message buffer when data is valid' +
+       '\n\tExpectation: Raw message buffer should be returned',
     function(done) {
-      var testBuffer = new Buffer(10);
-      testBuffer[0] = zutil.SOF;
-      testBuffer[1] = 6;
-      testBuffer[2] = zutil.REQUEST;
-      testBuffer[3] = 0x03;
-      testBuffer[4] = 0xAC;
-      testBuffer[5] = 0xFE;
-      testBuffer[6] = 0xFF;
-      testBuffer[7] = zutil.calcCS(testBuffer.slice(1,7), 6); // CS
 
-      var message = new baseMessage.getInstance(testBuffer, 8);
-
+      var message = new baseMessage.getInstance(null, 0);
       expect(message).not.equal(null);
 
-      var messageData = message.getData();
+      message.mValid = true;
+      message.mType = zutil.REQUEST;
+      message.mFunctionID = 0x04;
+      message.mDataBufferLen = 5;
+      message.mDataBuffer = new Buffer(20);
+      message.mDataBuffer[0] = 0xAA;
+      message.mDataBuffer[1] = 0xAB;
+      message.mDataBuffer[2] = 0xAC;
+      message.mDataBuffer[3] = 0xAD;
+      message.mDataBuffer[4] = 0x11;
 
-      expect(messageData).not.equal(null);
-      expect(messageData['type']).to.equal(zutil.REQUEST);
-      expect(messageData['functionID']).to.equal(0x03);
-      expect(messageData['data']).not.equal(null);
-      expect(messageData['data'][0]).to.equal(0xAC);
-      expect(messageData['data'][1]).to.equal(0xFE);
-      expect(messageData['data'][2]).to.equal(0xFF);
-      expect(messageData['dataLen']).to.equal(3);
-      expect(messageData['callbackID']).to.equal(zutil.INVALID_CALLBACK_ID);
-      expect(messageData['cs']).to.equal(testBuffer[7]);
-      expect(messageData['valid']).to.equal(true);
+      var messageRawBuffer = message.generateMessageBuffer();
+
+      expect(messageRawBuffer).not.be(Error);
+      expect(messageRawBuffer).not.equal(null);
+      expect(messageRawBuffer.length).to.equal(10);
+      expect(messageRawBuffer[0]).to.equal(zutil.SOF);
+      expect(messageRawBuffer[1]).to.equal(8);
+      expect(messageRawBuffer[2]).to.equal(zutil.REQUEST);
+      expect(messageRawBuffer[3]).to.equal(0x04);
+      expect(messageRawBuffer[4]).to.equal(0xAA);
+      expect(messageRawBuffer[5]).to.equal(0xAB);
+      expect(messageRawBuffer[6]).to.equal(0xAC);
+      expect(messageRawBuffer[7]).to.equal(0xAD);
+      expect(messageRawBuffer[8]).to.equal(0x11);
+      expect(messageRawBuffer[9]).to.equal(0xE2);
 
       done();
     });
 
+  test('generateMessageBuffer valid message' +
+       '\n\tPurpose: Attempt to generate the message buffer when data is valid' +
+       '\n\tExpectation: Raw message buffer should be returned',
+    function(done) {
 
+      var message = new baseMessage.getInstance(null, 0);
+      expect(message).not.equal(null);
+
+      message.mValid = true;
+      message.mType = zutil.REQUEST;
+      message.mFunctionID = 0x04;
+      message.mDataBufferLen = 5;
+      message.mDataBuffer = new Buffer(20);
+      message.mDataBuffer[0] = 0xAA;
+      message.mDataBuffer[1] = 0xAB;
+      message.mDataBuffer[2] = 0xAC;
+      message.mDataBuffer[3] = 0xAD;
+      message.mDataBuffer[4] = 0x11;
+
+      var messageRawBuffer = message.generateMessageBuffer();
+
+      expect(messageRawBuffer).not.be(Error);
+      expect(messageRawBuffer).not.equal(null);
+      expect(messageRawBuffer.length).to.equal(10);
+      expect(messageRawBuffer[0]).to.equal(zutil.SOF);
+      expect(messageRawBuffer[1]).to.equal(8);
+      expect(messageRawBuffer[2]).to.equal(zutil.REQUEST);
+      expect(messageRawBuffer[3]).to.equal(0x04);
+      expect(messageRawBuffer[4]).to.equal(0xAA);
+      expect(messageRawBuffer[5]).to.equal(0xAB);
+      expect(messageRawBuffer[6]).to.equal(0xAC);
+      expect(messageRawBuffer[7]).to.equal(0xAD);
+      expect(messageRawBuffer[8]).to.equal(0x11);
+      expect(messageRawBuffer[9]).to.equal(0xE2);
+
+      done();
+    });
 
   test('setData no type' +
        '\n\tPurpose: Pass message info without type' +
@@ -431,5 +470,31 @@ suite('BaseMessage', function() {
       done();
     });
 
+  test('setData valid' +
+       '\n\tPurpose: Pass message info which results in a message that is valid' +
+       '\n\tExpectation: An error should be returned',
+    function(done) {
+      var message = new baseMessage.getInstance(null, 8);
 
+      expect(message).not.equal(null);
+
+      var messageData = [];
+      messageData['type'] = 0x00;
+      messageData['functionID'] = 0x03;
+      var data  = new Buffer(5);
+      messageData['data'] = data;
+      data[0] = 1;
+      data[1] = 2;
+      data[2] = 3;
+      data[3] = 4;
+      data[4] = 5;
+      messageData['dataLen'] = 5;
+
+      var result = message.setData(messageData);
+
+      expect(result).to.equal(true);
+      expect(message.mCS).to.equal(0xF5)
+
+      done();
+    });
 });
